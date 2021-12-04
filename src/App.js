fa12f20ceb4cb84f66ff2dc49e75aceded91ba49
Mahammad Sharifov <azeridesigner@gmail.com>
2021-12-05 00:31:06 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
 
   getWeather = async(e) => {
     e.preventDefault();
-    const Api_Key = '0fd4e3f60360adcf8c74cdfd1bfca6f2';  
+    const Api_Key = 'XXXXXXXXXXXXXX';  
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`);
